459f1d58adde82cd25f586846bc812b044d22875
mbugua <johnmbugua23code@gmail.com>
2026-02-19 15:26:21 +0300

    update: added fonts (proxymanovabold and proxymanovalight)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const proximaNova = localFont({
+  src: [
+    {
+      path: "../public/proxymanova/Manova/Web Fonts/584e3c862a2f3de781da5da2233357df.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-proxima-nova",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${proximaNova.variable} antialiased`}
       >
         {children}
       </body>
