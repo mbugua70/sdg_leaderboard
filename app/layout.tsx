e163a6b15863b08fba65875d9ebdf8e6371667de
mbugua <johnmbugua23code@gmail.com>
2026-02-19 15:38:49 +0300

    update: applied font to the code

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,18 @@ import "./globals.css";
 const proximaNova = localFont({
   src: [
     {
+      path: "../public/proxmanovalight/ProximaNova-Light/Web Fonts/90b3680786db32f9b5c112d1537677db.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
       path: "../public/proxymanova/Manova/Web Fonts/584e3c862a2f3de781da5da2233357df.woff2",
       weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/proxymanovabold/ProximaNova-Bold/Web Fonts/f697c472b2ad5a1ca383a63c349f3dd9.woff2",
+      weight: "700",
       style: "normal",
     },
   ],
